da8a761762a416e357e91d96311db535ebf77fb5
Irtiza Ali <aliartiza75@yahoo.com>
2019-02-10 00:37:49 +0500

    [implementing-fe] code structure has been added

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
-from app.navigator.routes import mod
+from app.ht_fe.routes import mod
 from flask import Flask, Blueprint
 from flask_api import FlaskAPI
 app = Flask(__name__)
 
 # registering application
-app.register_blueprint(mod, uri_prefix='/navigator')
+app.register_blueprint(mod, uri_prefix='/ht_fe')
